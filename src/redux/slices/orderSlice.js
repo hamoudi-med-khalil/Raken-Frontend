b4eb6c0812a19c1a45b51e8a931b9ebe5348abba
mohamed_khalil_hamoudi <mohamedkhalilhamoudi1@gmail.com>
2025-06-04 00:30:59 +0200

    redux Slices setup

---
+++ src/redux/slices/orderSlice.js
@@ -0,0 +1,80 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const fetchUserOrders = createAsyncThunk("orders/fetchUserOrders",
+    async (__dirname, { rejectWithValue }) => {
+        try {
+            const response = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/api/orders/my-ordes`,
+                {
+                    headers: {
+                        Authorization: `Bearer ${localStorage.getItem("userToken")}`
+                    }
+                }
+
+            )
+            return response.data
+        } catch (error) {
+            console.error(error);
+            return rejectWithValue(error.response.data)
+        }
+    }
+)
+
+// Async thunc fetch orders details by id
+
+export const fetchOrderById = createAsyncThunk("orders/fetchsingleorder", async(orderId ,{rejectWithValue})=>{
+    try {
+        const response = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/api/orders/${orderId}`,
+           {
+             headers : {
+                Authorization : `Bearer ${localStorage.getItem("userToken")}`
+            }}
+        )
+        return response.data
+    } catch (error) {
+            console.error(error);
+            return rejectWithValue(error.response.data)        
+    }
+})
+
+const orderSlice = createSlice({
+    name: "order",
+    initialState :{
+        orders : [],
+        totalOrders : 0,
+        orderDetails: null,
+        loading : false,
+        error : null,
+    },
+    reducers : {},
+    extraReducers : (builder) => {
+        builder
+        .addCase(fetchUserOrders.pending , (state)=>{
+            state.loading = true,
+            state.error = null;
+        })
+        .addCase(fetchUserOrders.fulfilled , (state, action)=>{
+            state.loading = false,
+            state.orders = action.payload
+        })
+        .addCase(fetchUserOrders.rtejected , (state, action)=>{
+            state.loading = false,
+            state.error = action.payload.message
+        })
+        .addCase(fetchOrderById.pending , (state)=>{
+            state.loading = true,
+            state.error = null;
+        })
+        .addCase(fetchOrderById.fulfilled , (state, action)=>{
+            state.loading = false,
+            state.orderDetails = action.payload
+        })
+        .addCase(fetchOrderById.rtejected , (state, action)=>{
+            state.loading = false,
+            state.error = action.payload.message
+        })
+
+    }
+})
+
+export default orderSlice.reducer
